73a44e23e00f6083ad9d19c104941b57bed5479c
azraelrabbit <rock_82@outlook.com>
2017-11-10 23:41:35 +0800

    add x86 test demo

---
+++ For-linux/sample.linux.x86/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using ChromiumFX;
+using ChromiumFX.Extension;
+using Chromium.WebBrowser;
+
+namespace ceftestf
+{
+    public class Form1:System.Windows.Forms.Form
+    {
+
+        ChromiumWebBrowser browser;
+
+        public Form1()
+        {
+            this.Width = 900;
+            this.Height = 700;
+            this.InitLayout();
+
+             
+        }
+
+           protected override void OnLoad(EventArgs e)
+        {
+            //base.OnLoad(e);
+
+            browser = new ChromiumWebBrowser("https://www.baidu.com", this);
+
+            browser.Dock = DockStyle.Fill;
+
+
+            browser.LoadUrl("http://www.baidu.com/");
+                                
+        }
+    }
+}
---
+++ For-linux/sample.linux.x86/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using ChromiumFX;
+using ChromiumFX.Extension;
+using Chromium;
+using Chromium.WebBrowser;
+using System.IO;
+
+namespace ceftestf
+{
+    class MainClass
+    {
+        public static void Main(string[] args){
+
+ 
+            var basepath = Environment.CurrentDirectory;
+ 
+
+   //         var cefPath = System.IO.Path.Combine(basepath, "mycef");
+   //         var cfxPath = System.IO.Path.Combine(basepath, "mycef");
+
+   //         ChromiumStartup.LibCefPath = cefPath;
+   //         ChromiumStartup.LibCfxPath = cfxPath;
+
+			//ChromiumStartup.CefResourcePath = Path.Combine(basepath, "mycef");// or Path.Combine(basePath, "cef","Resource");
+
+			ChromiumStartup.Initialize();
+
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new Form1());
+        }
+    }
+}
